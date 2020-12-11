000dab47796fde27160dec327d45df09fdbb5000
js0807 <batjs@naver.com>
2020-12-11 20:49:15 +0900

    Update build_vbox.bat to work with setup.py

--- script/build_vbox.bat
+++ script/build_vbox.bat
@@ -17,6 +17,9 @@ SET QT_BUILD_DIR=%DEFAULT_DIR%\Qt\qt5-x64
 echo [+]VirtualBox
 cd /d %VBOX_DIR%
 
-echo  [-]VirtualBox Build Setting
+echo [-]VirtualBox Build Setting
 set BUILD_TARGET_ARCH=amd64
 cscript configure.vbs --with-vc="%VS2010VC_DIR%" --with-qt5="%DEFAULT_DIR%\Qt\qt5-x64" --with-DDK="%WINDDK_DIR%" --with-MinGW-w64="%MINGW_DIR%" --with-libSDL="%SDL_DIR%" --with-openssl="%SSL64_DIR%" --with-openssl32="%SSL32_DIR%" --with-libcurl="%DEFAULT_DIR%\curl\x64" --with-libcurl32="%DEFAULT_DIR%\curl\x32"
+
+
+echo [+]VirtualBox Build Setting Done
