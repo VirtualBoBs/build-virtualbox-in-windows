03912bc87e1dd77a506d24961f6629b4a95395ef
jidoc01 <jidoc01@naver.com>
2020-12-14 13:56:06 +0900

    Check privilege and test mode in build.py This code chunk comes from setup.py

--- script/build.py
+++ script/build.py
@@ -1,6 +1,13 @@
 from tool import *
 
 def main():
+    if is_admin() == False:
+        print('[*] Error: Please run this script on the root privilege')
+        exit(1)
+    if is_test_mode() == False:
+        print('[*] Error: Please run this script on the test mode')
+        exit(1)
+
     # Build VBox
     os.chdir(path_vbox_dir)
     execute_batch_x32_inst('call env.bat\nkmk')
