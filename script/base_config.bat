2f8ef8dc7be09d2a1d0cf18f3691a5caaca48429
jidoc01 <jidoc01@naver.com>
2020-12-14 11:46:43 +0900

    Use absolute path in base_config.bat Now, it does not use relative path anymore for its stability.

--- script/base_config.bat
+++ script/base_config.bat
@@ -1,7 +1,19 @@
+@echo off
+
 SET VS2010VC_DIR=C:\Program Files (x86)\Microsoft Visual Studio 10.0
 SET WINDDK_DIR=C:\WinDDK\7600.16385.1\bin\amd64
 
 SET DEFAULT_DIR=%cd%\..\bin
+
+rem // Save current directory and change to target directory
+pushd %DEFAULT_DIR%
+
+rem // Save value of CD variable (current directory)
+set DEFAULT_PATH=%CD%
+
+rem // Restore original directory
+popd
+
 SET MINGW_DIR=%DEFAULT_DIR%\MinGW\mingw64
 SET SDL_DIR=%DEFAULT_DIR%\SDL
 SET SSL64_DIR=%DEFAULT_DIR%\SSL\OpenSSL-Win64
